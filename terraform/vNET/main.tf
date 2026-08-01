507568c777d3fa5840599cf58e679b24df74f31f
venkatramanpalanikumar <venkatramanpalanikumar@gmail.com>
2026-08-01 11:02:27 -0400

    Addign Subnet

--- terraform/vNET/main.tf
+++ terraform/vNET/main.tf
@@ -47,3 +47,10 @@ resource "azurerm_subnet" "db" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_db_prefix]
 }
+
+resource "azurerm_subnet" "devaps" {
+  name                 = "subnet-devaps"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_devaps_prefix]
+}
