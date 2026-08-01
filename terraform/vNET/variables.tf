0c475bca3793ef3fa7fc72e2128d6ac0b2ffc320
venkatramanpalanikumar-lgtm <venkatramanpalanikumar@gmail.com>
2026-08-01 10:21:46 -0400

    Update variables.tf

--- terraform/vNET/variables.tf
+++ terraform/vNET/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-dev"
+  default     = "terraformResearchGroup"
 }
 
 variable "location" {
